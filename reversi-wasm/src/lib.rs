572ecbba85e03dea5c61b471bef61805042e807a
Takamasa Oshikiri <mail@oshikiri.org>
2020-08-23 23:29:17 +0900

    Add initial Board impl

--- reversi-wasm/src/lib.rs
+++ reversi-wasm/src/lib.rs
@@ -11,11 +11,49 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, reversi-wasm!");
+}
+
+type BitBoard = u64;
+
+#[derive(Debug)]
+struct Board {
+    first: BitBoard,  // black, 先手
+    second: BitBoard, // white, 後手
+}
+
+impl Board {
+    #[allow(dead_code)]
+    fn is_full(&self) -> bool {
+        (self.first | self.second) == BitBoard::MAX
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use Board;
+
+    #[test]
+    fn is_full_should_return_false_when_board_is_empty() {
+        let board = Board {
+            first: 0,
+            second: 0,
+        };
+        assert_eq!(board.is_full(), false);
+    }
+
+    #[test]
+    fn is_full_should_return_true_when_board_is_full() {
+        let board = Board {
+            first: 0,
+            second: u64::MAX,
+        };
+        assert_eq!(board.is_full(), true);
+    }
 }
