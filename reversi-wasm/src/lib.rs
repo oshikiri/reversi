0247535aa122b24fddc6cc6a22f01cbf3d22dd75
Takamasa Oshikiri <mail@oshikiri.org>
2020-08-24 20:53:25 +0900

    Add skeltons of count_bits and is_valid

--- reversi-wasm/src/lib.rs
+++ reversi-wasm/src/lib.rs
@@ -33,11 +33,22 @@ impl Board {
     fn is_full(&self) -> bool {
         (self.first | self.second) == BitBoard::MAX
     }
+
+    #[allow(dead_code)]
+    fn is_valid(&self) -> bool {
+        true
+    }
+}
+
+#[allow(dead_code)]
+pub fn count_bits(_bitboard: BitBoard) -> u64 {
+    64
 }
 
 #[cfg(test)]
 mod tests {
     use Board;
+    use count_bits;
 
     #[test]
     fn is_full_should_return_false_when_board_is_empty() {
@@ -55,5 +66,11 @@ mod tests {
             second: u64::MAX,
         };
         assert_eq!(board.is_full(), true);
+    }
+
+    #[test]
+    fn count_bits_should_return_num_of_bits() {
+        // assert_eq!(count_bits(0), 0);
+        assert_eq!(count_bits(u64::MAX), 64);
     }
 }
