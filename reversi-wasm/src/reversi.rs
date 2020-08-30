73af53bb713c352fa933382f45e0242baf590faa
Takamasa Oshikiri <mail@oshikiri.org>
2020-08-30 19:01:32 +0900

    Set #[allow(non_snake_case)] to newBoard

--- reversi-wasm/src/reversi.rs
+++ reversi-wasm/src/reversi.rs
@@ -11,6 +11,7 @@ pub struct Board {
 }
 
 #[wasm_bindgen]
+#[allow(non_snake_case)]
 pub fn newBoard() -> Board {
     Board {
         first: 0b_00000000_00000000_00000000_00010000_00001000_00000000_00000000_00000000,
