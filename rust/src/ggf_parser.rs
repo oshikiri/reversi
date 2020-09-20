a4371b3b475402eb81fe16bd5887b3b717970eec
Takamasa Oshikiri <mail@oshikiri.org>
2020-09-20 21:03:55 +0900

    Implement B, W

--- rust/src/ggf_parser.rs
+++ rust/src/ggf_parser.rs
@@ -3,14 +3,23 @@ pub struct Game {
     first_name: String,
     second_name: String,
     game_type: String,
+    moves: Vec<(char, char, char)>,
 }
 
+type CharTriple = (char, char, char);
+
 #[allow(dead_code)] // TODO: remove
-pub fn new_game(first_name_str: &str, second_name_str: &str, game_type_str: &str) -> Game {
+pub fn new_game(
+    first_name_str: &str,
+    second_name_str: &str,
+    game_type_str: &str,
+    moves: Vec<CharTriple>,
+) -> Game {
     Game {
         first_name: first_name_str.to_string(),
         second_name: second_name_str.to_string(),
         game_type: game_type_str.to_string(),
+        moves,
     }
 }
 
@@ -28,9 +37,21 @@ fn comsume_until_close_bracket(chars: &Vec<char>, i: usize) -> (usize, String) {
     (j, content)
 }
 
+// fn parse_move_content(turn: char, i: usize, chars: Vec<char>, content: String) -> Option<CharTriple> {
+//   let first_slash = content.find('/').unwrap_or(content.len());
+//   let first_element = content.get(0..first_slash).unwrap();
+//   if first_element != "pass" {
+//       let x = chars.get(i + 1).unwrap();
+//       let y = chars.get(i + 2).unwrap();
+//       Some((turn, *x, *y))
+//   } else {
+//     None
+//   }
+// }
+
 #[allow(dead_code)] // TODO: remove
 pub fn parse(game_string: String) -> Game {
-    let mut game = new_game("", "", "");
+    let mut game = new_game("", "", "", Vec::new());
 
     let mut buffer = String::from("");
     let chars = game_string.chars().collect::<Vec<char>>();
@@ -68,6 +89,30 @@ pub fn parse(game_string: String) -> Game {
                 game.game_type = game_type;
                 buffer.clear();
             }
+            "B[" => {
+                let (i_next, content) = comsume_until_close_bracket(&chars, i + 1);
+                let first_slash = content.find('/').unwrap_or(content.len());
+                let first_element = content.get(0..first_slash).unwrap();
+                if first_element != "pass" {
+                    let x = chars.get(i + 1).unwrap();
+                    let y = chars.get(i + 2).unwrap();
+                    game.moves.push(('B', *x, *y));
+                }
+                i = i_next;
+                buffer.clear();
+            }
+            "W[" => {
+                let (i_next, content) = comsume_until_close_bracket(&chars, i + 1);
+                let first_slash = content.find('/').unwrap_or(content.len());
+                let first_element = content.get(0..first_slash).unwrap();
+                if first_element != "pass" {
+                    let x = chars.get(i + 1).unwrap();
+                    let y = chars.get(i + 2).unwrap();
+                    game.moves.push(('W', *x, *y));
+                }
+                i = i_next;
+                buffer.clear();
+            }
             _ => {
                 if c == ']' {
                     println!("{}", buffer);
@@ -93,10 +138,20 @@ mod tests {
     fn parse_game_01e4_1() {
         // https://www.skatgame.net/mburo/ggs/game-archive/Othello/
         // bzgrep . Othello.01e4.ggf.bz2 | head -1
-        let game_string = String::from("(;GM[Othello]PC[GGS/os]DT[2000-4-16 11:13 EST]PB[fangr]PW[patzer]RB[1457.12]RW[1631.74]TI[15:00//02:00]TY[8]RE[-40.00]BO[8 -------- -------- -------- ---O*--- ---*O--- -------- -------- -------- *]B[E6//4.09]W[F4/-48.50/0.93]B[C3//5.52]W[C4/-42.50/0.43]B[D3//1.70]W[D6/-39.50/0.28]B[F6//4.31]W[C6/-28.55/0.13]B[F5//2.17]W[G5/0.55/0.10]B[G6//2.21]W[E3/-30.57/0.02]B[F2//1.65]W[F7/-43.63/0.01]B[E7//4.71]W[F3/-43.63]B[H5//1.80]W[E2/-43.63]B[C5//5.10]W[B6/0.63]B[B4//6.88]W[B5/2.82]B[B3//8.30]W[A4/2.04]B[A3//3.06]W[H4/-2.55]B[H3//8.17]W[D8/-5.22/0.01]B[C7//9.61]W[D7/-2.40]B[C8//51.08]W[E8/0.90]B[C2//17.00]W[G4/4.05]B[F1//16.62]W[H6/6.88/0.01]B[G3//11.69]W[H2/12.51/0.01]B[A5//33.91]W[D2/15.19/0.01]B[E1//2.67]W[B2/13.64/0.01]B[B7//7.38]W[D1/15.94/0.01]B[G2//5.81]W[A6/15.89/0.01]B[A7//3.84]W[H1/27.21/0.01]B[F8//6.78]W[A8/29.49/0.01]B[B8//3.80]W[A2/29.39/0.01]B[A1//2.96]W[G1/30.62/0.01]B[C1//3.05]W[B1/30.46/0.01]B[H7//5.06]W[H8/40.00/0.01]B[pass//1.67]W[G7/40.00/0.01]B[pass//2.10]W[G8];)");
+        let game_string = String::from("(;GM[Othello]PC[GGS/os]DT[2000-4-16 11:13 EST]PB[fangr]PW[patzer]RB[1457.12]RW[1631.74]TI[15:00//02:00]TY[8]RE[-40.00]BO[8 -------- -------- -------- ---O*--- ---*O--- -------- -------- -------- *]B[E6//4.09]W[H8/40.00/0.01]B[pass//1.67]W[G7/40.00/0.01]B[pass//2.10]W[G8];)");
 
         let actual = parse(game_string);
-        let expected = new_game("fangr", "patzer", "8");
+        let expected = new_game(
+            "fangr",
+            "patzer",
+            "8",
+            vec![
+                ('B', 'E', '6'),
+                ('W', 'H', '8'),
+                ('W', 'G', '7'),
+                ('W', 'G', '8'),
+            ],
+        );
         assert_eq!(actual, expected);
     }
 }
