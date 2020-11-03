cb3116a1ccea616ef9c99a3ab19ce63039779d47
Takamasa Oshikiri <mail@oshikiri.org>
2020-11-03 22:56:49 +0900

    Use alphabeta in PatternLookahead1Strategy

--- rust/src/strategy.rs
+++ rust/src/strategy.rs
@@ -1,11 +1,9 @@
 use wasm_bindgen::prelude::*;
 
 use crate::bitboard;
-use crate::board::{count_bits, Board};
-use crate::console_log;
+use crate::board::Board;
 use crate::player::Player;
 use crate::search_algorithm::alphabeta::AlphaBeta;
-use crate::search_algorithm::base::GameTreeLeaf;
 
 #[wasm_bindgen]
 #[derive(Debug)]
@@ -59,51 +57,25 @@ impl Strategy for NumdiskLookaheadStrategy {
 pub struct PatternLookahead1Strategy {}
 
 impl Strategy for PatternLookahead1Strategy {
-    // TODO: 高速化
     fn get_next_move(
         &mut self,
         board: &Board,
         player: &Player,
         _i_step: usize,
     ) -> Result<(Option<u64>, f32), String> {
+        let mut alphabeta =
+            AlphaBeta::create(1000000000, |board: &Board, player: &Player| -> f32 {
+                let pattern_instance_indices =
+                    bitboard::extract_pattern_instance_indices(board, player);
+                Board::calculate_pattern_score(pattern_instance_indices)
+            });
         let root_board = match player {
             Player::First => board.clone(),
             Player::Second => Board::reverse(&board),
         };
-
-        let mut leaves: Vec<GameTreeLeaf> = Vec::new();
-        for i_cell in 0..64 {
-            let put_position = 1 << i_cell;
-            let reverse_pattern =
-                board.get_reverse_pattern(root_board.first(), root_board.second(), put_position);
-            if count_bits(reverse_pattern) <= 0 {
-                continue;
-            }
-
-            let mut next_board = root_board.clone();
-            next_board.put_and_reverse(&player, put_position);
-
-            let pattern_instance_indices =
-                bitboard::extract_pattern_instance_indices(&next_board, &player);
-            let pattern_score = Board::calculate_pattern_score(pattern_instance_indices);
-            let leaf =
-                GameTreeLeaf::create(player.clone(), pattern_score, vec![Some(put_position)]);
-            leaves.push(leaf);
-        }
-
-        leaves.sort_by(|l, r| {
-            r.score()
-                .partial_cmp(&l.score())
-                .unwrap_or(std::cmp::Ordering::Equal)
-        });
-
-        console_log!("  leaves: {:?}", leaves);
-
-        if leaves.is_empty() {
-            Err(String::from("leaves is empty"))
-        } else {
-            let best_leaf = leaves[0].clone();
-            Ok((best_leaf.moves()[0], best_leaf.score()))
+        match alphabeta.search(&root_board, 0) {
+            Some((Some(best_move), score)) => Ok((Some(best_move), score)),
+            _ => Err(String::from("Result of alpha-beta pruning search is empty")),
         }
     }
 }
