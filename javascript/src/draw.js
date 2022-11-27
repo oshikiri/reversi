69d3eb7e5a339f4e8c01ba83ef3c7a2bd992e352
Takamasa Oshikiri <mail@oshikiri.org>
2022-11-27 21:05:39 +0900

    Refactor draw.js

--- javascript/src/draw.js
+++ javascript/src/draw.js
@@ -7,26 +7,24 @@ export function initializeBoard() {
 }
 
 function createCell(r, c) {
-  const cell = document.createElement("div");
-  cell.classList = "cell";
-  cell.classList.add("empty");
+  const disk = createDiv("disk");
+  disk.appendChild(createDiv("disk-front"));
+  disk.appendChild(createDiv("disk-back"));
+
+  const cell = createDiv("cell", "empty");
   cell.dataset.boardRow = r;
   cell.dataset.boardColumn = c;
-
-  const diskFront = document.createElement("div");
-  diskFront.classList = "disk-front";
-
-  const diskBack = document.createElement("div");
-  diskBack.classList = "disk-back";
-
-  const disk = document.createElement("div");
-  disk.classList = "disk";
-  disk.appendChild(diskFront);
-  disk.appendChild(diskBack);
-
   cell.appendChild(disk);
 
   return cell;
+}
+
+function createDiv() {
+  const div = document.createElement("div");
+  for (const arg of arguments) {
+    div.classList.add(String(arg));
+  }
+  return div;
 }
 
 export function renderBoard(bitboardFirst, bitboardSecond, i, j) {
@@ -35,7 +33,7 @@ export function renderBoard(bitboardFirst, bitboardSecond, i, j) {
     const c = k % 8;
     activateCell(r, c, r == i && c == j);
   }
-  drawDisks(bitboardFirst, bitboardSecond);
+  updateDisks(bitboardFirst, bitboardSecond);
 }
 
 function activateCell(i, j, isActive) {
@@ -53,7 +51,7 @@ function getCell(i, j) {
   );
 }
 
-const drawDisks = (first, second) => {
+function updateDisks(firstBidboard, secondBitboard) {
   let firstScore = 0;
   let secondScore = 0;
 
@@ -61,18 +59,18 @@ const drawDisks = (first, second) => {
     const i = k % 8;
     const j = Math.floor(k / 8);
 
-    if (first[k] == 1) {
+    if (firstBidboard[k] == 1) {
       firstScore++;
-      drawDisk(i, j, true);
-    } else if (second[k] == 1) {
+      updateDiskIsFirst(i, j, true);
+    } else if (secondBitboard[k] == 1) {
       secondScore++;
-      drawDisk(i, j, false);
+      updateDiskIsFirst(i, j, false);
     }
   }
   document.querySelector("#scores").innerHTML = `${firstScore}-${secondScore}`;
-};
+}
 
-const drawDisk = (i, j, isFirst) => {
+function updateDiskIsFirst(i, j, isFirst) {
   const cell = getCell(i, j);
   cell.classList.remove("empty");
   if (isFirst) {
@@ -80,4 +78,4 @@ const drawDisk = (i, j, isFirst) => {
   } else {
     cell.classList.add("flipped");
   }
-};
+}
