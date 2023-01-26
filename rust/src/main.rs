44805bc84060f7a5177ed6a86dd3943529c66244
Takamasa Oshikiri <mail@oshikiri.org>
2023-01-26 22:53:23 +0900

    Update benchmark command

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -39,10 +39,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 - - - - - - - -
             ",
             );
-            let mut alphabeta = AlphaBeta::create(10000, |board: &Board, player: &Player| -> f32 {
+            let mut alphabeta = AlphaBeta::create(1000000, |board: &Board, player: &Player| -> f32 {
                 board.score_numdisk(player)
             });
-            let search_results = alphabeta.search(&current_board, 7);
+            let search_results = alphabeta.search(&current_board, 9);
             println!("{:?}", search_results);
 
             Ok(())
